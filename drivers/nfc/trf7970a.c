a695659e32758e9bc2f1a6fabb25dc75facc1ffb
Mark A. Greer <mgreer@animalcreek.com>
2014-04-07 12:52:21 -0700

    NFC: trf7970a: Only reset FIFO when no more TX bytes left
    Commit d0a0397c45240cb7e0b9e2cdd192c412b5df5a19
    (NFC: trf7970a: Reset FIFO when 'End of TX' Interrupt Occurs)
    unconditionally resets the FIFO when the trf7970a generates
    an interrupt with the TX bit set.  However, an interrupt with
    that bit set can be generated when the TX is not complete and
    the FIFO just went past its low watermark.  In this case, the
    FIFO should not be cleared (as it would destroy the remainder
    of the TX frame) and instead just ignore the interrupt.  Once
    the TX is complete, the trf7970a will generate another interrupt
    with the TX bit set but not the FIFO bit so the FIFO can be
    cleared then.
    
    Signed-off-by: Mark A. Greer <mgreer@animalcreek.com>

--- drivers/nfc/trf7970a.c
+++ drivers/nfc/trf7970a.c
@@ -641,7 +641,7 @@ static irqreturn_t trf7970a_irq(int irq, void *dev_id)
 {
 	struct trf7970a *trf = dev_id;
 	int ret;
-	u8 status;
+	u8 status, fifo_bytes;
 
 	mutex_lock(&trf->lock);
 
@@ -694,6 +694,18 @@ static irqreturn_t trf7970a_irq(int irq, void *dev_id)
 			trf->ignore_timeout =
 				!cancel_delayed_work(&trf->timeout_work);
 			trf7970a_drain_fifo(trf, status);
+		} else if (status & TRF7970A_IRQ_STATUS_FIFO) {
+			ret = trf7970a_read(trf, TRF7970A_FIFO_STATUS,
+					&fifo_bytes);
+			if (ret) {
+				trf7970a_send_err_upstream(trf, ret);
+			} else if (fifo_bytes & TRF7970A_FIFO_STATUS_OVERFLOW) {
+				dev_err(trf->dev, "%s - fifo overflow: 0x%x\n",
+						__func__, fifo_bytes);
+				trf7970a_send_err_upstream(trf, -EIO);
+			} else if (!fifo_bytes) {
+				trf7970a_cmd(trf, TRF7970A_CMD_FIFO_RESET);
+			}
 		} else if (status == TRF7970A_IRQ_STATUS_TX) {
 			trf7970a_cmd(trf, TRF7970A_CMD_FIFO_RESET);
 		} else {
